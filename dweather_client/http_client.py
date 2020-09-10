fcf8c5b58cbede957469b18589acf19392122213
Ben Andre <bandre@gmail.com>
2020-09-10 16:25:31 -0400

    Add revision tagged getter functions.

--- dweather_client/http_client.py
+++ dweather_client/http_client.py
@@ -4,6 +4,7 @@ Basic functions for getting data from a dWeather gateway via https.
 import requests, datetime, io, gzip
 from dweather_client.ipfs_errors import *
 from dweather_client.utils import listify_period
+import IPFSDatasets
 
 MM_TO_INCHES = 0.0393701
 RAINFALL_PRECISION = 5
@@ -106,7 +107,7 @@ def get_zipped_hash_cell(url, hash_str, coord_str):
         return zip_data.read().decode("utf-8")
 
 
-def get_dataset_cell(lat, lon, dataset):
+def get_dataset_cell(lat, lon, dataset_revision):
     """ 
     Retrieve the text of a grid cell data file for a given lat lon and dataset.
     Args:
@@ -120,30 +121,30 @@ def get_dataset_cell(lat, lon, dataset):
         CoordinateNotFoundError: If the lat/lon coordinate is not found on server
     """
     all_hashes = get_heads()
-    if dataset in all_hashes:
-        dataset_hash = all_hashes[dataset]
+    if dataset_revision in all_hashes:
+        dataset_hash = all_hashes[dataset_revision]
     else:
-        raise DatasetError('{} not found on server'.format(dataset))
+        raise DatasetError('{} not found on server'.format(dataset_revision))
 
     metadata = get_metadata(dataset_hash)
     min_lat, max_lat = sorted(metadata["latitude range"])
     min_lon, max_lon = sorted(metadata["longitude range"])
     if lat < min_lat or lat > max_lat:
-        raise InputOutOfRangeError("Latitude {} out of dataset range [{:.3f}, {:.3f}] for {}".format(lat, min_lat, max_lat, dataset))
+        raise InputOutOfRangeError("Latitude {} out of dataset revision range [{:.3f}, {:.3f}] for {}".format(lat, min_lat, max_lat, dataset_revision))
     if  lon < min_lon or lon > max_lon:
-        raise InputOutOfRangeError("Longitude {} out of dataset range [{:.3f}, {:.3f}] for {}".format(lon, min_lon, max_lon, dataset))
+        raise InputOutOfRangeError("Longitude {} out of dataset revision range [{:.3f}, {:.3f}] for {}".format(lon, min_lon, max_lon, dataset_revision))
     coord_str = "{:.3f}_{:.3f}".format(lat,lon)
     try:
         if "compression" in metadata and metadata["compression"] == "gzip":
-            text_data = get_zipped_hash_cell(server_url, dataset_hash, coord_str)
+            text_data = get_zipped_hash_cell(GATEWAY_URL, dataset_hash, coord_str)
         else:
             text_data = get_hash_cell(dataset_hash, coord_str)
         return metadata, text_data
     except requests.exceptions.RequestException as e:
-        raise CoordinateNotFoundError('Coordinate ({}, {}) not found  on ipfs in dataset {}'.format(lat, lon, dataset))
+        raise CoordinateNotFoundError('Coordinate ({}, {}) not found  on ipfs in dataset revision {}'.format(lat, lon, dataset_revision))
 
 
-def get_rainfall_dict(lat, lon, dataset, return_metadata=False):
+def get_rainfall_dict(lat, lon, dataset_revision, return_metadata=False):
     """ 
     Build a dict of rainfall data for a given grid cell.
     Args:
@@ -157,7 +158,7 @@ def get_rainfall_dict(lat, lon, dataset, return_metadata=False):
         CoordinateNotFoundError: If the lat/lon coordinate is not found on server
         DataMalformedError: If the grid cell file can't be parsed as rainfall data
     """
-    metadata, rainfall_text = get_dataset_cell(lat, lon, dataset)
+    metadata, rainfall_text = get_dataset_cell(lat, lon, dataset_revision)
     dataset_start_date = datetime.datetime.strptime(metadata['date range'][0], "%Y/%m/%d").date()
     dataset_end_date = datetime.datetime.strptime(metadata['date range'][1], "%Y/%m/%d").date()
     timedelta = dataset_end_date - dataset_start_date
@@ -174,7 +175,44 @@ def get_rainfall_dict(lat, lon, dataset, return_metadata=False):
         return rainfall_dict
 
 
-def get_temperature_dict(lat, lon, dataset, return_metadata=False):
+def get_rev_rainfall_dict(lat, lon, dataset, desired_end_date, latest_rev):
+    """
+    Build a dictionary of rainfall data. Include as much of the most accurate, final data as possible. Start by buidling from the most accurate data,
+    then keep appending data from more recent/less accurate versions of the dataset until we run out or reach the end date.
+
+    This will not throw an error if there are no revisions with data available, it will simply return what is available.
+    Args:
+        lat (float): the grid cell latitude
+        lon (float): the grid cell longitude
+        dataset (str): the name of the dataset, e.g., "chirps_05-daily" on hashes.json
+        desired_end_date (datetime.date): the last day of data needed.
+        latest_rev (str): the least accurate revision of the dataset that is considered final
+    Returns:
+        tuple:
+            a dict ({datetime.date: float}) of datetime dates and the corresponding rainfall in mm for that date
+            bool is_final: if all data up to desired end date is final, this will be true
+    """
+    all_rainfall = {}
+    is_final = True
+
+    # Build the rainfall from the most accurate revision of the dataset to the least
+    for dataset_revision in IPFSDatasets.datasets[dataset]:
+        additional_rainfall = get_rainfall_dict(lat, lon, dataset_revision)
+        all_dates = list(all_rainfall) + list(additional_rainfall)
+        # This method of dict comprehension preserves the order of the dict
+        all_rainfall = {date: all_rainfall[date] if date in all_rainfall else additional_rainfall[date] for date in all_dates}
+        # stop when we have the desired end date in the dataset
+        if desired_end_date in all_rainfall:
+            return all_rainfall, is_final
+        # data is no longer final after we pass the specified version
+        if dataset_revision == latest_rev:
+            is_final = False
+
+    # If we don't reach the desired dataset, return all data.
+    return all_rainfall, is_final
+
+
+def get_temperature_dict(lat, lon, dataset_revision, return_metadata=False):
     """
     Build a dict of temperature data for a given grid cell.
     Args:
@@ -185,16 +223,16 @@ def get_temperature_dict(lat, lon, dataset, return_metadata=False):
         highs: dict ({datetime.date: float}) of datetime dates and the corresponding high temperature in degress F
         lows: dict ({datetime.date: float}) of datetime dates and the corresponding low temperature in degress F
     Raises:
-        DatasetError: If no matching dataset found on server
-        InputOutOfRangeError: If the lat/lon is outside the dataset range in metadata
+        DatasetError: If no matching dataset_revision found on server
+        InputOutOfRangeError: If the lat/lon is outside the dataset_revision range in metadata
         CoordinateNotFoundError: If the lat/lon coordinate is not found on server
         DataMalformedError: If the grid cell file can't be parsed as temperature data
     """
-    metadata, temp_text = get_dataset_cell(lat, lon, dataset)
+    metadata, temp_text = get_dataset_cell(lat, lon, dataset_revision)
     dataset_start_date = datetime.datetime.strptime(metadata['date range'][0], "%Y/%m/%d").date()
     dataset_end_date = datetime.datetime.strptime(metadata['date range'][1], "%Y/%m/%d").date()
     timedelta = dataset_end_date - dataset_start_date
-    days_in_record = timedelta.days + 1 # we have both the start and end date in the dataset so its the difference + 1
+    days_in_record = timedelta.days + 1 # we have both the start and end date in the dataset_revision so its the difference + 1
     day_strs = temp_text.replace(',', ' ').split()
     if (len(day_strs) != days_in_record):
         raise DataMalformedError ("Number of days in data file does not match the provided metadata")
@@ -209,3 +247,71 @@ def get_temperature_dict(lat, lon, dataset, return_metadata=False):
         return metadata, highs, lows
     else:
         return highs, lows
+
+
+def get_rev_temperature_dict(lat, lon, dataset, desired_end_date, latest_rev):
+    """
+    Build a dictionary of rainfall data. Include as much final data as possible. If the desired end date
+    is not in the final dataset, append as much prelim as possible.
+    Args:
+        lat (float): the latitude of the grid cell, to 3 decimals
+        lon (float): the longitude of the grid cell, to 3 decimals
+        dataset (str): the dataset name as on hashes.json
+        desired_end_date (datetime.date): don't include prelim data after this point if not needed
+        latest_rev (str): The least accurate revision that is still considered 'final'
+    returns:
+        tuple (highs, lows) of dicts and a bool
+        highs: dict ({datetime.date: float}) of datetime dates and the corresponding high temperature in degress F
+        lows: dict ({datetime.date: float}) of datetime dates and the corresponding low temperature in degress F
+        is_final: True if all data is from final dataset, false if prelim included
+    """
+    highs = {}
+    lows = {}
+    is_final = True
+
+    # Build the data from the most accurate version of the dataset to the least
+    for dataset_revision in IPFSDatasets.datasets[dataset]:
+        additional_highs, additional_lows = get_temperature_dict(lat, lon, dataset_revision)
+        all_dates = list(highs) + list(additional_highs)    
+        highs = {date: highs[date] if date in highs else additional_highs[date] for date in all_dates}
+        lows = {date: lows[date] if date in lows else additional_lows[date] for date in all_dates}
+        # Stop early if we have the end date
+        if desired_end_date in highs:
+            return highs, lows, is_final
+
+        # data is no longer final after we pass the specified version
+        if dataset_revision == latest_rev:
+            is_final = False
+
+    # If we don't reach the desired dataset, return all data.
+    return highs, lows, is_final
+
+
+def get_rev_tagged_temperature_dict(lat, lon, dataset, desired_end_date=None):
+    ''' Build temps with a revision tag by each date
+    Args:
+        lat (float): the grid cell latitude
+        lon (float): the grid cell longitude
+        dataset (str): the dataset name in ipfs
+        desired_end_date (datetime.date): stop early if we get this end date
+    returns
+        highs: dict with keys of dates, values are tuple (temperature, revision tag) for that date
+        lows: dict with keys of dates, values are tuple (temperature, revision tag) for that date
+    '''
+    highs = {}
+    lows = {}
+
+    # Build the data from the most accurate version of the dataset to the least
+    for dataset_version in IPFSDatasets.datasets[dataset]:
+        additional_highs, additional_lows = get_temperature_dict(lat, lon, dataset_version)
+        all_dates = list(highs) + list(additional_highs)    
+        highs = {date: highs[date] if date in highs else (additional_highs[date], dataset_version) for date in all_dates}
+        lows = {date: lows[date] if date in lows else (additional_lows[date], dataset_version) for date in all_dates}
+        # Stop early if we have the end date
+        if desired_end_date in highs:
+            return highs, lows
+
+    # If we don't reach the desired dataset, return all data.
+    return highs, lows
+
+
--- dweather_client/tests/test_http_client.py
+++ dweather_client/tests/test_http_client.py
@@ -1,18 +1,22 @@
 import http_client
 import utils
+from datetime import date
 
 def test_http_client_rainfall():
-    dataset = 'chirps_05-daily'
+    dataset = 'chirps_05'
+    dataset_revision = 'chirps_05-daily'
     heads = http_client.get_heads()
-    chirps_metadata = http_client.get_metadata(heads[dataset])
-    rainfall_dict = http_client.get_rainfall_dict(41.175, -75.125, dataset)
-
+    chirps_metadata = http_client.get_metadata(heads[dataset_revision])
+    rainfall_dict = http_client.get_rainfall_dict(41.175, -75.125, dataset_revision)
+    rainfall_rev_dict = http_client.get_rev_rainfall_dict(41.175, -75.125, dataset, date.today(), dataset_revision)
 
 def test_http_client_temperature():
-    dataset = 'cpc_temp-daily'
+    dataset = "cpc_temp"
+    dataset_revision = 'cpc_temp-daily'
     lat, lon = utils.conventional_lat_lon_to_cpc(41.25, -77.75)
     heads = http_client.get_heads()
-    cpc_metadata = http_client.get_metadata(heads[dataset])
-    temperature_dict = http_client.get_temperature_dict(lat, lon, dataset)
-
+    cpc_metadata = http_client.get_metadata(heads[dataset_revision])
+    temperature_dict = http_client.get_temperature_dict(lat, lon, dataset_revision)
+    temperature_rev_dict = http_client.get_rev_temperature_dict(lat, lon, dataset, date.today(), dataset_revision)
+    tagged_temperature_rev_dict = http_client.get_rev_tagged_temperature_dict(lat, lon, dataset)
 

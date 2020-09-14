7e1ded26d3847a6e24b9099532c6e3591cd18758
Mackenzie <macmikk@gmail.com>
2020-09-14 14:01:28 -0500

    Separated out utils functions that are using pandas for uploading to lambda

---
+++ dweather_client/df_utils.py
@@ -0,0 +1,80 @@
+"""
+Helpful auxilliary functions that don't directly interact with IPFS.
+Basically, it should go here if it doesn't need to import from df_loader or http_client,
+this module uses pandas
+"""
+import math
+import pandas as pd
+import datetime
+
+def cpc_grid_to_lat_lon(grid):
+    """ 
+    Convert a cpc grid id to conventional lat lon via a lookup table.
+    return:
+        latitude, longitude
+        
+    args:
+        grid = "1100" example
+    
+    """
+    cpc_grids =  pd.read_csv(os.path.join(CPC_LOOKUP_PATH)).set_index('Grid ID') #dataframe of cpc grid to lat/lon lookup table
+    myGrid = cpc_grids.iloc[grid]
+    coords = [myGrid["Latitude"], myGrid["Longitude"]]
+    coords = [coord+360 if coord < 0 else coord for coord in coords] #converts negative coordinates to positive values
+    
+    return coords[0], coords[1]
+
+
+def icao_to_ghcn(icao):
+    """ 
+    Convert an icao airport code to ghcn.
+    return:
+        latitude, longitude
+    args:
+        icao = "xxxx" for example try "KLGA"
+    """
+    icao_codes = pd.read_csv(os.path.join(ICAO_LOOKUP_PATH)).set_index('ICAO') #get lookup table
+    return icao_codes.loc[icao]["GHCN"]
+
+def period_slice_df(df, ps, pe, yrs):
+    """ 
+    Slice a dataframe by period where n is based on years lookback 
+    
+    return:
+        pd.DataFrame()
+    
+    args:
+        df = any pd.DataFrame() with a datetime.date() index
+        ps = datetime.date object (contract period start)
+        ps = datetime.date object (contract period end)
+        yrs = 30 (years lookback)
+        
+    limitations:
+        max period is 364 days
+        
+    """
+
+    mydf = pd.DataFrame()
+    
+    leftDay = ~((df.index.day < ps.day) & (df.index.month == ps.month))
+    rightDay = ~((df.index.day > pe.day) & (df.index.month == pe.month))
+    #first year just get end
+    if ps.year < pe.year:
+        firstYear = ps.year - yrs
+        yr = df.index.year == firstYear
+        mon = df.index.month >= ps.month
+        #a boolean mask to cut the left and right half month days if need be
+        mydf = mydf.append(df[(yr)&(mon)&(leftDay)])
+        #everything in betweeng
+        for yrss in range(ps.year - (yrs - 1), pe.year - 1):
+            ##boolean mask for now, better method needed##
+            yearChunk = (df.index.year == yrss)&((df.index.month<=pe.month)|(df.index.month>=ps.month))
+            yearChunk = (yearChunk)&leftDay&rightDay
+            mydf = mydf.append(df[yearChunk])
+        #build last year
+        mydf = mydf.append(df[rightDay&(df.index.month<=pe.month)&(df.index.year==pe.year-1)])
+    else:
+        mydf = df[leftDay&rightDay&(df.index.month>=ps.month)&(df.index.month<=pe.month)&(df.index.year>=ps.year-yrs)]
+            
+    return mydf
+
--- dweather_client/utils.py
+++ dweather_client/utils.py
@@ -1,13 +1,9 @@
 """
 Helpful auxilliary functions that don't directly interact with IPFS.
-Basically, it should go here if it doesn't need to import from df_loader or http_client,
+This module specifically excludes pandas
 """
 import math
-import pandas as pd
 import datetime
-
-ICAO_LOOKUP_PATH = 'etc/airport-codes.csv'
-CPC_LOOKUP_PATH = 'etc/cpc-grid-ids.csv'
 
 def snap_to_grid(lat, lon, metadata):
     """ 
@@ -53,36 +49,6 @@ def conventional_lat_lon_to_cpc(lat, lon):
     return lat, lon + 360
 
 
-def cpc_grid_to_lat_lon(grid):
-    """ 
-    Convert a cpc grid id to conventional lat lon via a lookup table.
-    return:
-        latitude, longitude
-        
-    args:
-        grid = "1100" example
-    
-    """
-    cpc_grids =  pd.read_csv(os.path.join(CPC_LOOKUP_PATH)).set_index('Grid ID') #dataframe of cpc grid to lat/lon lookup table
-    myGrid = cpc_grids.iloc[grid]
-    coords = [myGrid["Latitude"], myGrid["Longitude"]]
-    coords = [coord+360 if coord < 0 else coord for coord in coords] #converts negative coordinates to positive values
-    
-    return coords[0], coords[1]
-
-
-def icao_to_ghcn(icao):
-    """ 
-    Convert an icao airport code to ghcn.
-    return:
-        latitude, longitude
-    args:
-        icao = "xxxx" for example try "KLGA"
-    """
-    icao_codes = pd.read_csv(os.path.join(ICAO_LOOKUP_PATH)).set_index('ICAO') #get lookup table
-    return icao_codes.loc[icao]["GHCN"]
-
-
 def listify_period(start_date, end_date):
     """
     Make a list of all dates from start_date to end_date, inclusive.
@@ -94,49 +60,6 @@ def listify_period(start_date, end_date):
     """
     days_in_range = (end_date - start_date).days
     return [start_date + datetime.timedelta(n) for n in range(days_in_range + 1)]
-
-
-def period_slice_df(df, ps, pe, yrs):
-    """ 
-    Slice a dataframe by period where n is based on years lookback 
-    
-    return:
-        pd.DataFrame()
-    
-    args:
-        df = any pd.DataFrame() with a datetime.date() index
-        ps = datetime.date object (contract period start)
-        ps = datetime.date object (contract period end)
-        yrs = 30 (years lookback)
-        
-    limitations:
-        max period is 364 days
-        
-    """
-
-    mydf = pd.DataFrame()
-    
-    leftDay = ~((df.index.day < ps.day) & (df.index.month == ps.month))
-    rightDay = ~((df.index.day > pe.day) & (df.index.month == pe.month))
-    #first year just get end
-    if ps.year < pe.year:
-        firstYear = ps.year - yrs
-        yr = df.index.year == firstYear
-        mon = df.index.month >= ps.month
-        #a boolean mask to cut the left and right half month days if need be
-        mydf = mydf.append(df[(yr)&(mon)&(leftDay)])
-        #everything in betweeng
-        for yrss in range(ps.year - (yrs - 1), pe.year - 1):
-            ##boolean mask for now, better method needed##
-            yearChunk = (df.index.year == yrss)&((df.index.month<=pe.month)|(df.index.month>=ps.month))
-            yearChunk = (yearChunk)&leftDay&rightDay
-            mydf = mydf.append(df[yearChunk])
-        #build last year
-        mydf = mydf.append(df[rightDay&(df.index.month<=pe.month)&(df.index.year==pe.year-1)])
-    else:
-        mydf = df[leftDay&rightDay&(df.index.month>=ps.month)&(df.index.month<=pe.month)&(df.index.year>=ps.year-yrs)]
-            
-    return mydf
 
 
 def is_revision_final(dataset, revision_to_check, last_acceptable_revision):

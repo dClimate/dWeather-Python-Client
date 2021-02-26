0a7069da11d7c336f2a564e59e8c7eb6b9d6a54f
Ben Andre <bandre@gmail.com>
2021-02-26 00:59:25 -0500

    Get dataframes working with station.

--- dweather_client/client.py
+++ dweather_client/client.py
@@ -4,6 +4,7 @@ Use these functions to get historical climate data.
 from dweather_client.http_queries import get_station_csv
 from dweather_client.aliases_and_units import STATION_COLUMN_LOOKUP as SCL, STATION_UNITS_LOOKUP as SUL
 import csv, pint, datetime
+import pandas as pd
 
 def get_gridcell_history(
     lat, 
@@ -50,20 +51,20 @@ def get_station_history( \
     station_id, 
     columns,
     dataset='ghcnd', 
-    protocol='https',
+#    protocol='https', TODO
     return_result_as_dataframe=False,
 #    also_return_metadata=False,  TODO
     use_imperial_units=True):
     """
-    Takes in a station id and a column name or iterable of column names. 
+    Takes in a station id and a column name or iterable of column names.
 
     Gets the csv body associated with the station_id, defaulting to the
     ghcnd dataset. Pass in dataset='ghcnd-imputed-daily' for imputed,
-    though note that ghcndi is temperature only as of this writing.
+    though note that ghcndi is only temperature as of this writing.
 
-    Passing in use_imperial=False will return results in metric. Imperial
-    is the default as Arbol is based in the USA and the bulk of our deals
-    are done in imperial.
+    Passing in use_imperial_units=False will return results in metric. 
+    Imperial is the default as Arbol is based in the USA and the bulk of our 
+    deals are done in imperial.
 
         'SNWD' or alias 'snow depth' -- the depth of snow at the time of the
         observation
@@ -80,10 +81,7 @@ def get_station_history( \
     aliases.
 
     """
-    if return_result_as_dataframe == True:
-        df = pd.read_csv(io.StringIO(get_station_csv(station_id, station_dataset=station_dataset)))
-    else:
-        csv_text = get_station_csv(station_id, station_dataset=dataset)
+    csv_text = get_station_csv(station_id, station_dataset=dataset)
     variables = ()
     for aliases in SCL:
         if columns in aliases:
@@ -93,14 +91,14 @@ def get_station_history( \
             for column in columns:
                 if column in aliases:
                     variables = variables + SCL[aliases] # otherwise assume it's an iterable of strings
-    dict_results = []
-    unit_reg = pint.UnitRegistry()
+    dict_results = {}
     for variable in variables:
-        unit_reg.default_format = SUL[variable]['precision']
-        data_col = column_names.index(variable)
         reader = csv.reader(csv_text.split('\n'))
         column_names = next(reader)
         date_col = column_names.index('DATE')
+        unit_reg = pint.UnitRegistry()
+        unit_reg.default_format = SUL[variable]['precision']
+        data_col = column_names.index(variable)
         data = {}
         for row in reader:
             try:
@@ -115,11 +113,23 @@ def get_station_history( \
             if use_imperial_units:
                 datapoint = datapoint.to(SUL[variable]['imperial'])
             data[datetime.datetime.strptime(row[date_col], "%Y-%m-%d").date()] = datapoint
-        if return_result_as_dataframe == False:
-            results.append(data)
-        else:
-            
-
-    return results if len(results) != 1 else results[0]    
-
-
+        dict_results[variable] = data
+    
+    if return_result_as_dataframe == False:
+        # return only {date: observation} if a single column is passed in.
+        return dict_results if len(dict_results) != 1 else dict_results[variables[0]]    
+    else:
+        final_df = None
+        for variable in dict_results:
+            intermediate_dict = {}
+            intermediate_dict["DATE"] = [date for date in dict_results[variable]]
+            intermediate_dict[variable] = [dict_results[variable][date] for date in dict_results[variable]]
+            df = pd.DataFrame.from_dict(intermediate_dict)
+            df.DATE = pd.to_datetime(df.DATE)
+            df.index = df["DATE"]
+            df.drop(df.columns[0], axis=1, inplace=True)
+            try:
+                final_df = final_df.merge(df, how="outer", on="DATE", sort=True)
+            except AttributeError:
+                final_df = df
+        return final_df
--- dweather_client/tests/test_client.py
+++ dweather_client/tests/test_client.py
@@ -1,8 +1,11 @@
 from dweather_client.client import get_station_history
 from dweather_client.aliases_and_units import snotel_to_ghcnd
+import pandas as pd
 
 def test_station():
-     get_station_dict('USW00014820', 'SNOW')
-     station_id = snotel_to_ghcnd(838, 'CO')
-     get_station_dict(station_id, 'snow water equivalent')
-     get_station_history('USW00014820', 'temperature')
+    get_station_history('USW00014820', 'SNOW')
+    station_id = snotel_to_ghcnd(838, 'CO')
+    get_station_history(station_id, 'snow water equivalent')
+    temp_history = get_station_history('USW00014820', 'temperature')
+    assert temp_history['TMIN'] != temp_history['TMAX']
+    get_station_history('USW00014820', ('SNOW', 'SNWD', 'TMIN'), return_result_as_dataframe=True)

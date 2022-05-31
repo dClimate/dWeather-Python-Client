624d3e1aeef3c8d4fc979d8d3f3c121110fabf56
arbolKiran <kiran@arbolmarket.com>
2022-05-31 12:40:23 +0100

    add test for cwv

--- dweather_client/tests/test_client.py
+++ dweather_client/tests/test_client.py
@@ -3,7 +3,7 @@ from dweather_client.tests.mock_fixtures import get_patched_datasets
 from dweather_client.client import get_australia_station_history, get_station_history, get_gridcell_history, get_tropical_storms,\
     get_yield_history, get_irrigation_data, get_power_history, get_gas_history, get_alberta_power_history, GRIDDED_DATASETS, has_dataset_updated,\
     get_forecast_datasets, get_forecast, get_cme_station_history, get_european_station_history, get_hourly_european_station_history, get_drought_monitor_history, get_japan_station_history,\
-    get_afr_history
+    get_afr_history, get_cwv_station_history
 from dweather_client.aliases_and_units import snotel_to_ghcnd
 import pandas as pd
 from io import StringIO
@@ -282,6 +282,11 @@ def test_japan_units():
     data = get_japan_station_history("Fukuoka", desired_units="K", ipfs_timeout=IPFS_TIMEOUT)
     assert len(data) > 19000
     assert data[sorted(data)[0]].unit == u.K
+
+def test_cwv():
+    data = get_cwv_station_history("EM", ipfs_timeout=IPFS_TIMEOUT)
+    assert len(data) == (sorted(data)[-1] - sorted(data)[0]).days + 1
+    assert data[sorted(data)[0]].unit == u.dimensionless_unscaled
 
 def test_australia():
     data = get_australia_station_history("Adelaide Airport", weather_variable="TMAX", ipfs_timeout=IPFS_TIMEOUT)

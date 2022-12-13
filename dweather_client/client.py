1aa1d5b1936dab54a0dc4a6b73e498bb793dbbc7
PrettyHertz <PrettyHertz@github.com>
2022-12-13 16:43:48 -0600

    Modified client, added tests
    Modified client to return as dictionary and not as the csv file object.

--- dweather_client/client.py
+++ dweather_client/client.py
@@ -653,11 +653,25 @@ def has_dataset_updated(dataset, slices, as_of, ipfs_timeout=None):
         ranges = dataset_obj.get_data(as_of)
     return has_changed(slices, ranges)
 
-def get_teleconnections_history(ipfs_timeout=None):
+def get_teleconnections_history(weather_variable, ipfs_timeout=None):
     with TeleconnectionsDataset(ipfs_timeout=ipfs_timeout) as dataset_obj:
         csv_text = dataset_obj.get_data()
-        return csv_text
-        #reader = csv.reader(csv_text.split('\n'))
-        #headers = next(reader)
-        #date_col = headers.index('Date')
-        #for row in reader:
+        history = {}
+        reader = csv.reader(csv_text.split('\n'))
+        headers = next(reader)
+        date_col = headers.index('DATE')
+        try:
+            data_col = headers.index(weather_variable)
+        except ValueError:
+            raise WeatherVariableNotFoundError("Invalid weather variable for this station")
+        for row in reader:
+            try:
+                if row[data_col] == '':
+                    continue
+            except IndexError:
+                continue
+            try:
+                history[datetime.datetime.strptime(row[date_col], "%Y-%m-%d").date()] = float(row[data_col])
+            except:
+                history[datetime.datetime.strptime(row[date_col], "%Y-%m-%d").date()] = row[data_col]
+        return history
